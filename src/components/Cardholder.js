4129300d6464aff4320a63a8bcad06b32fb30e63
keshav-star <keshavsandhu00@gmail.com>
2022-09-26 08:23:41 +0530

    mata kondo

--- src/components/Cardholder.js
+++ src/components/Cardholder.js
@@ -2,12 +2,12 @@ import Card from 'react-bootstrap/Card';
 import '../App.css'
 export function Expense({ title, amount }) {
   return (
-    <div className="text-center w-50 m-auto">
+    <Card className="text-center w-50 m-auto">
       <div className='d-flex p-2'>
         <h4 className='title text-uppercase pt-1 '>{title}</h4>
         <button type="button" className='amount btn btn-danger ms-auto '>₹ {amount}</button>
       </div>
-    </div>
+    </Card>
   )
 }
 export function Fund({ title, amount }) {
